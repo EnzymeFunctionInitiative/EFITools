29944f1a629043f8fea874f68d83f4d9bcccedaf
Nils Oberg <noberg@illinois.edu>
2019-07-26 11:11:50 -0500

    Added function for checking SSN type (e.g. UniRef, Domain)

--- lib/EFI/Util.pm
+++ lib/EFI/Util.pm
@@ -8,7 +8,7 @@ use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
 $VERSION     = 1.00;
 @ISA         = qw(Exporter);
 @EXPORT      = ();
-@EXPORT_OK   = qw(usesSlurm getSchedulerType getLmod defaultScheduler validateConfigFile);
+@EXPORT_OK   = qw(usesSlurm getSchedulerType getLmod defaultScheduler validateConfigFile checkNetworkType);
 
 
 sub usesSlurm {
@@ -44,6 +44,35 @@ sub getLmod {
     my @py2 = grep m{$pattern}, (split m/[\n\r]+/s, $err);
 
     return scalar @py2 ? $py2[0] : $default;
+}
+
+sub checkNetworkType {
+    my $file = shift;
+    my ($type, $isDomain) = ("UniProt", 0);
+
+    if ($file =~ m/.zip$/) {
+        $file = "unzip -c $file | sed '/<\\/node>/q' |";
+    }
+
+    my $success = open FILE, $file;
+    if (not $success) {
+        warn "Unable to scan input SSN $file for type: $!";
+        return (0, 0);
+    }
+
+    while (<FILE>) {
+        if (m/<node .*label="([^"]+)"/) {
+            $isDomain = ($1 =~ m/:/);
+        } elsif (m/<\/node/) {
+            last;
+        } elsif (m/<att .*UniRef(\d+) /) {
+            $type = "UniRef$1";
+        }
+    }
+
+    close FILE;
+
+    return ($type, $isDomain);
 }
 
 1;
