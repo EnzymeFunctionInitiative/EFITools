59fb34c3926be40040dace3142d1a44eeeedcc6a
Nils Oberg <noberg@illinois.edu>
2019-10-24 10:45:02 -0500

    Added summary tables and downloadable zip files for each consensus residue; also access HMMs directly instead of through master zip.

--- hmm/get_cluster_count.pl
+++ hmm/get_cluster_count.pl
@@ -18,6 +18,7 @@ my $result = GetOptions(
 die "Need size-file" if (not $sizeFile or not -f $sizeFile) and not $fastaDir;
 die "Need count-file" if not $countFile;
 
+my $hasMinCount = defined $minCount;
 $minCount = 5 if not $minCount;
 
 open COUNT, ">", $countFile or die "Unable to open $countFile for writing: $!";
@@ -25,7 +26,8 @@ open COUNT, ">", $countFile or die "Unable to open $countFile for writing: $!";
 if ($fastaDir and -d $fastaDir) {
     my @files = glob("$fastaDir/cluster_*.fasta");
     my @wc = map { my $c = `grep \\> $_ | wc -l`; chomp $c; (my $num = $_) =~ s/^.*cluster_(domain_)?(\d+)\.fasta$/$2/; [$num, $c] } @files;
-    map { print COUNT $_->[0], "\n" if $_->[1] >= $minCount; } @wc;
+    map { print COUNT $_->[0], "\n" if $_->[1] >= $minCount; } @wc if $hasMinCount;
+    map { print COUNT $_->[0], "\t", $_->[1], "\n"; } @wc if not $hasMinCount;
 } else {
     open SIZE, $sizeFile or die "Unable to open $sizeFile for writing: $!";
     scalar <SIZE>; #discard header
