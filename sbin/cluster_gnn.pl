9447da7e6cc06e4718a40c5730369564952057cd
Nils Oberg <noberg@illinois.edu>
2019-11-20 18:33:35 -0600

    Added ability to exclude fragments from GNN/Color SSN.

--- sbin/cluster_gnn.pl
+++ sbin/cluster_gnn.pl
@@ -31,7 +31,7 @@ my ($ssnin, $neighborhoodSize, $warningFile, $gnn, $ssnout, $cooccurrence, $stat
     $uniprotIdDir, $uniprotDomainIdDir, $uniref50IdDir, $uniref90IdDir, $uniref50DomainIdDir, $uniref90DomainIdDir,
     $pfamCoocTable, $hubCountFile, $allPfamDir, $splitPfamDir, $allSplitPfamDir, $clusterSizeFile, $swissprotClustersDescFile,
     $swissprotSinglesDescFile, $parentDir, $renumberClusters, $disableCache, $skipIdMapping, $skipOrganism, $debug,
-    $outputDir,
+    $outputDir, $excludeFragments,
 );
 
 my $result = GetOptions(
@@ -70,6 +70,7 @@ my $result = GetOptions(
     "disable-cache"         => \$disableCache,
     "skip-id-mapping"       => \$skipIdMapping,
     "skip-organism"         => \$skipOrganism,
+    "exclude-fragments"     => \$excludeFragments,
     "debug"                 => \$debug,
 );
 
@@ -563,6 +564,9 @@ sub getClusterToIdMapping {
             (my $proteinId = $id) =~ s/:\d+:\d+$//;
             if ($ssnSeqSource) {
                 my $sql = "SELECT * FROM uniref WHERE accession = '$proteinId'";
+                if ($excludeFragments) {
+                    $sql = "SELECT U.* FROM uniref AS U LEFT JOIN annotations AS A ON U.accession = A.accession WHERE U.accession = '$proteinId' AND A.Fragment = 0";
+                }
                 my $sth = $dbh->prepare($sql);
                 $sth->execute;
                 while (my $row = $sth->fetchrow_hashref) {
@@ -570,8 +574,19 @@ sub getClusterToIdMapping {
                     $uniref90IdsClusterMap{$row->{uniref90_seed}} = $clNum if $ssnSeqSource >= 50;
                 }
             }
-            $uniprotIdsClusterMap{$proteinId} = $clNum;
-            $uniprotDomainIdsClusterMap{$id} = $clNum if $id ne $proteinId;
+            
+            my $addSeq = 1;
+            if ($excludeFragments) {
+                $addSeq = 1;
+                my $sql = "SELECT Fragment FROM annotations WHERE accession = '$proteinId' AND Fragment = 0";
+                my $sth = $dbh->prepare($sql);
+                $sth->execute;
+                $addSeq = $sth->fetchrow_hashref ? 1 : 0;
+            }
+            if ($addSeq) {
+                $uniprotIdsClusterMap{$proteinId} = $clNum;
+                $uniprotDomainIdsClusterMap{$id} = $clNum if $id ne $proteinId;
+            }
         }
     }
 
@@ -773,6 +788,8 @@ sub defaultParameters {
     $arrowDataFile = "" if not $arrowDataFile;
     $pfamCoocTable = "" if not $pfamCoocTable;
     $hubCountFile = "" if not $hubCountFile;
+
+    $excludeFragments = defined($excludeFragments);
 }
 
 
